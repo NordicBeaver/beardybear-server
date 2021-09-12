cc681e05eac31ea1143218df2b1f3ca381436c67
NordicBeaver <nordicbeaver@gmail.com>
2021-09-12 14:07:59 +0300

    BarberDto and findOne method.

--- src/barbers/barbers.controller.ts
+++ src/barbers/barbers.controller.ts
@@ -1,5 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { Barber } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+interface BarberDto {
+  id: number;
+  name: string;
+  description: string;
+}
+
+function barberToDto(barber: Barber) {
+  const dto: BarberDto = {
+    id: barber.id,
+    name: barber.name,
+    description: barber.description,
+  };
+  return dto;
+}
 
 @Controller('barbers')
 export class BarbersController {
@@ -8,6 +31,19 @@ export class BarbersController {
   @Get()
   async findAll() {
     const barbers = await this.prisma.barber.findMany();
-    return barbers;
+    const barbersDto = barbers.map(barberToDto);
+    return barbersDto;
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) barberId: number) {
+    const barber = await this.prisma.barber.findFirst({
+      where: { id: barberId },
+    });
+    if (barber == null) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+    const barberDto = barberToDto(barber);
+    return barberDto;
   }
 }
