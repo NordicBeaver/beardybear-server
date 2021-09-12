4f7a7d0298e95200f272609695ad2b9272c1bfbd
NordicBeaver <nordicbeaver@gmail.com>
2021-09-12 14:07:58 +0300

    Use prisma to retrieve barbers.

--- src/barbers/barbers.controller.ts
+++ src/barbers/barbers.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Controller('barbers')
 export class BarbersController {
   @Get()
   async findAll() {
-    return [
-      {
-        id: 0,
-        name: 'Best Barber',
-      },
-      {
-        id: 1,
-        name: "Best Barber's friend",
-      },
-    ];
+    const client = new PrismaClient();
+    const barbers = await client.barber.findMany();
+    return barbers;
   }
 }
