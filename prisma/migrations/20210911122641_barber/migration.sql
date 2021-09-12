236b9fc5866fdfa9cc938d9c1a922d5b342016bc
NordicBeaver <nordicbeaver@gmail.com>
2021-09-12 14:07:58 +0300

    Add 'Barber' model.

---
+++ prisma/migrations/20210911122641_barber/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Barber" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Barber_pkey" PRIMARY KEY ("id")
+);
