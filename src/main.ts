a5dc4cc98586b1735d20d0c5c5e89b7329ef12ae
NordicBeaver <nordicbeaver@gmail.com>
2021-09-30 12:54:58 +0300

    Enable CORS.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
   await app.listen(3000);
