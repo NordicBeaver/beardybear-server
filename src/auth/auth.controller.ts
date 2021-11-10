71fc823d289a08a139cb3c5825c6cec83504f97e
NordicBeaver <nordicbeaver@gmail.com>
2021-11-10 11:09:57 +0300

    Check credentials from database on login.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,9 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { scrypt } from 'crypto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { promisify } from 'util';
 
 class Credentials {
   @IsString()
@@ -20,24 +23,35 @@ class Credentials {
 
 @Controller('auth')
 export class AuthController {
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService, private prisma: PrismaService) {}
 
   @Post('/login')
-  login(@Body() { username, password }: Credentials) {
-    if (username === 'admin' && password === 'password') {
-      const userId = 1;
-      const payload = {
-        sub: userId,
-      };
-      const token = this.jwtService.sign(payload);
-      return {
-        accessToken: token,
-      };
-    } else {
-      throw new HttpException(
-        'Username/password are not valid.',
-        HttpStatus.UNAUTHORIZED,
-      );
+  async login(@Body() { username, password }: Credentials) {
+    const invalidCredentialsError = new HttpException(
+      'Username/password are not valid.',
+      HttpStatus.UNAUTHORIZED,
+    );
+
+    const user = await this.prisma.user.findFirst({
+      where: { name: username },
+    });
+    if (user == null) {
+      throw invalidCredentialsError;
     }
+
+    const hashBuffer = (await promisify(scrypt)(
+      password,
+      user.passwordSalt,
+      64,
+    )) as Buffer;
+    const hash = hashBuffer.toString('hex');
+    if (hash !== user.passwordHash) {
+      throw invalidCredentialsError;
+    }
+
+    const token = this.jwtService.sign({ sub: user.id });
+    return {
+      accessToken: token,
+    };
   }
 }
